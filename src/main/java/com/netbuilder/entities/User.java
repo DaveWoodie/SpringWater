8a55ab79b900e002cbbbdda8fbd1a91394056e56
unknown <dwoodward@PC-CZC53003GX.netbuilder.private>
2015-10-06 15:47:33 +0100

    Quick add of the first entity, no getters or setters yet but you should now all have the right projects.

---
+++ src/main/java/com/netbuilder/entities/User.java
@@ -0,0 +1,24 @@
+package com.netbuilder.entities;
+
+public class User {
+	
+	private int iDnum;
+	private String pWd;
+	private String forName;
+	private String surName;
+	private String email;
+	private boolean isEmp;
+	
+	public User(String pwd, String fore, String surn, String ema, boolean emp) {
+		
+		pWd = pwd;
+		forName = fore;
+		surName = surn;
+		email = ema;
+		isEmp = emp;		
+	}
+	
+	public void setPwd(String p) {
+		
+	}
+}
