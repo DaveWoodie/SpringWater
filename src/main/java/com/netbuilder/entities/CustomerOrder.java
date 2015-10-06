9e24d5b0350d322de0a7055ab35710654db120b1
unknown <abutcher@PC-CZC45100JG.netbuilder.private>
2015-10-06 16:44:25 +0100

    commit merge

---
+++ src/main/java/com/netbuilder/entities/CustomerOrder.java
@@ -0,0 +1,13 @@
+package com.netbuilder.entities;
+
+import java.util.Date;
+
+public class CustomerOrder 
+{
+	private int customerOrderID;
+	private Date datePlaced;
+	private Date dateShipped;
+	//private Customer customer;
+	//private OrderStatus orderStatus;
+	//private Employee employee;
+}
