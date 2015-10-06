6b7d68ebaa975a2fee4b769dd0220649e041b54b
Tom-Stacey <Tom.Stacey@netbuilder.com>
2015-10-06 17:26:30 +0100

    Address entity and Supplier Entity completed with full javadoc comments

--- src/main/java/com/netbuilder/entities/Address.java
+++ src/main/java/com/netbuilder/entities/Address.java
@@ -8,59 +8,136 @@ public class Address {
 	private String county;
 	private String postCode;
 	
+
 	/**
-	 * 
-	 * @param addressLineOne
-	 * @param addressLineTwo
-	 * @param city
-	 * @param county
-	 * @param postCode
+	 * Constructor for a new address with full details completed
+	 * @author tstacey
+	 * @date 06/10/2015
+	 * @param addressLineOne : String of the first line of the address (House number)
+	 * @param addressLineTwo : String of the second line of the address (Building/development)
+	 * @param city : String of the city name
+	 * @param county : String of the county of the address 
+	 * @param postCode : String of the address post code
 	 */
-	public Address(String addressLineOne, String city, String postCode) {
+	public Address(String addressLineOne, String addressLineTwo, String city, String county, String postCode) {
 		this.addressLineOne = addressLineOne;
+		this.addressLineTwo = addressLineTwo;
 		this.city = city;
+		this.county = county;
 		this.postCode = postCode;
 	}
 	
-	public void setAddressLineTwo(String lineTwo) {
-		this.addressLineTwo = lineTwo;
-	}
-	
-	public void setCounty(String county) {
+	/**
+	 * Constructor for a new address without addressLineTwo completed
+	 * @author tstacey
+	 * @date 06/10/2015
+	 * @param addressLineOne : String of the first line of the address (House number)
+	 * @param city : String of the city name
+	 * @param county : String of the county of the address 
+	 * @param postCode : String of the address post code
+	 */
+	public Address(String addressLineOne, String city, String county, String postCode) {
+		this.addressLineOne = addressLineOne;
+		this.city = city;
 		this.county = county;
+		this.postCode = postCode;
 	}
 	
+	/**
+	 * Blank constructor for a new address entity
+	 */
+	public Address() {
+		
+	}
+	
+	/**
+	 * Method to set the first line of the address (house number)
+	 * @param lineOne
+	 */
 	public void setAddressLineOne(String lineOne) {
 		this.addressLineOne = lineOne;
 	}
 	
+	/**
+	 * Method to set the second line of the address (Building/development)
+	 * @param lineTwo
+	 */
+	public void setAddressLineTwo(String lineTwo) {
+		this.addressLineTwo = lineTwo;
+	}
+	
+	/**
+	 * Method to set the city of the address
+	 * @param city
+	 */
 	public void setCity(String city) {
 		this.city = city;
 	}
 	
+	/**
+	 * Method to set the county of the address
+	 * @param county
+	 */
+	public void setCounty(String county) {
+		this.county = county;
+	}
+	
+	/**
+	 * Method to set the post code of the address
+	 * @param postCode
+	 */
 	public void setPostCode(String postCode) {
 		this.postCode = postCode;
 	}
 	
+	/**
+	 * method to get the ID of the Address
+	 * @return int of the Address ID
+	 */
+	public int getAddressID() {
+		return this.addressID;
+	}
+	
+	/**
+	 * Method to return the first line of the address (House Number)
+	 * @return String of the first line of the address
+	 */
 	public String getaddressLineOne() {
 		return this.addressLineOne;
 	}
 	
+	/**
+	 * Method to return the second line of the address (Building/development etc.)
+	 * @return String of the second line of the address
+	 */
 	public String getAddressLineTwo() {
 		return this.addressLineTwo;
 	}
 	
+	/**
+	 * MEthod to return the city of the address
+	 * @return String of the address city
+	 */
 	public String getCity() {
 		return this.city;
 	}
 	
+	/**
+	 * Method to return the county of the address
+	 * @return String of the address county
+	 */
 	public String getCounty() {
 		return this.county;
 	}
 	
+	/**
+	 * Method to return the post code of the address 
+	 * @return String of the address post code
+	 */
 	public String getPostCode() {
 		return this.postCode;
 	}
+	
 	
 }
 
