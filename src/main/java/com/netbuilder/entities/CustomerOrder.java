22dec1ae5961e1b00f495e7b1f836f408492c8bd
AndrewB94 <andrew.butcher@netbuilder.com>
2015-10-07 13:00:15 +0100

    add SQL persistence labels

--- src/main/java/com/netbuilder/entities/CustomerOrder.java
+++ src/main/java/com/netbuilder/entities/CustomerOrder.java
@@ -7,13 +7,27 @@ package com.netbuilder.entities;
 
 import java.util.Date;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+
 public class CustomerOrder 
 {
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private int customerOrderID;
 	private Date datePlaced;
 	private Date dateShipped;
+	@ManyToOne
+	@PrimaryKeyJoinColumn (name="idCustomer")
 	private Customer customer;
+	@ManyToOne
+	@PrimaryKeyJoinColumn (name="idCustomerOrderStatus")
 	private CustomerOrderStatus customerOrderStatus;
+	@ManyToOne
+	@PrimaryKeyJoinColumn (name="idEmployee")
 	private Employee employee;
 	
 	/**
@@ -25,8 +39,7 @@ public class CustomerOrder
 	 * @param orderStatus
 	 * @param employee
 	 */
-	public CustomerOrder(Date datePlaced, Customer customer, CustomerOrderStatus orderStatus, Employee employee)
-	{
+	public CustomerOrder(Date datePlaced, Customer customer, CustomerOrderStatus orderStatus, Employee employee) {
 		this.datePlaced = datePlaced;
 		this.customer = customer;
 		this.customerOrderStatus = orderStatus;
@@ -39,79 +52,42 @@ public class CustomerOrder
 	 * date 07/10/2015
 	 */
 	@Deprecated
-	public CustomerOrder() 
-	{
+	public CustomerOrder() {
 		
 	}
 
-	/**
-	 * Method to get the Customer Order ID.	
-	 * @return Returns an Int data type.
-	 */
 	public int getCustomerOrderID() {
 		return customerOrderID;
 	}
 
-	/**
-	 * Method to get the Date the order was placed.
-	 * @return Returns a Date data type.
-	 */
 	public Date getDatePlaced() {
 		return datePlaced;
 	}
 
-	/**
-	 * Method to get the Date the order was shipped.
-	 * @return Returns a Date data type.
-	 */
 	public Date getDateShipped() {
 		return dateShipped;
 	}
 
-	/**
-	 * Method to get the Customer who placed the order.
-	 * @return Returns a Customer data type.
-	 */
 	public Customer getCustomer() {
 		return customer;
 	}
 
-	/**
-	 * Method to get the Employee who is assigned to the order.
-	 * @return Returns an Employee data type.
-	 */
 	public Employee getEmployee() {
 		return employee;
 	}
 
-	/**
-	 * Method to get the customer order status.
-	 * @return Returns a CustomerOrderStatus data type.
-	 */
 	public CustomerOrderStatus getCustomerOrderStatus() {
 		return customerOrderStatus;
 	}
 	
-	/**
-	 * Method to set the Date the order was shipped.
-	 * @param dateShipped : Date of date shipped.
-	 */
 	public void setDateShipped(Date dateShipped) {
 		this.dateShipped = dateShipped;
 	}
 
-	/**
-	 * Method to set the last Employee assigned to the order.
-	 * @param employee : Employee to be assigned.
-	 */
 	public void setEmployee(Employee employee) {
 		this.employee = employee;
 	}
 	
-	/**
-	 * Method to set the current Order status
-	 * @param orderStatus : new order status.
-	 */
 	public void setOrderStatus(CustomerOrderStatus orderStatus) {
 		this.customerOrderStatus = orderStatus;
 	}
