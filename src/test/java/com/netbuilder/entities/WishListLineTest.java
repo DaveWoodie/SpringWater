bb2297b529f76b85173424d14ae6bb6405c76b7b
c33-cooper <Callum.Cooper@netbuilder.com>
2015-10-07 09:50:57 +0100

    JUnit test classes added for Customer and WishListLine, Next step is to implement them

---
+++ src/test/java/com/netbuilder/entities/WishListLineTest.java
@@ -0,0 +1,14 @@
+package com.netbuilder.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WishListLineTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
