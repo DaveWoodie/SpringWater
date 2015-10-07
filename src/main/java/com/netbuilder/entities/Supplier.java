0356833c3f7ce309c5084fa0e69cb8d1b66406ca
Tom-Stacey <Tom.Stacey@netbuilder.com>
2015-10-07 14:26:39 +0100

    Added SupplierManager class and updated Conventions document with conventions on Lists of entities

--- src/main/java/com/netbuilder/entities/Supplier.java
+++ src/main/java/com/netbuilder/entities/Supplier.java
@@ -1,12 +1,23 @@
 package com.netbuilder.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Class to hold all of the details for one Supplier
  * @author tstacey
  * @date 06/10/2015
  */
+
+@Entity
 public class Supplier {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private int supplierID;
+	
 	private String supplierName;
 	private String telephone;
 	private String email;
