3046d1b3993ff5c31d2dadbddf1c5f398b57991a
jameshforster <jamezx@hotmail.co.uk>
2015-10-07 11:29:10 +0100

    Finished unit test for purchase orders

--- src/main/java/com/netbuilder/entities/PurchaseOrder.java
+++ src/main/java/com/netbuilder/entities/PurchaseOrder.java
@@ -31,81 +31,53 @@ public class PurchaseOrder {
 	}
 	
 	/**
-	 * Blank constructor for testing purchase only
+	 * Blank constructor for testing purposes only
 	 */
 	@Deprecated
 	public PurchaseOrder() {
 		
 	}
-	/**
-	 * Method to get the purchase order's ID number
-	 * @return returns int data type
-	 */
+
 	public int getIDPurchaseOrder() {
 		return idPurchaseOrder;
 	}
 	
-	/**
-	 * Method to get the purchase order's date of creation
-	 * @return returns Date data type
-	 */
+
 	public Date getDatePlaced() {
 		return datePlaced;
 	}
 	
-	/**
-	 * Method to get the purchase order's expected arrival date
-	 * @return returns Date data type
-	 */
+
 	public Date getDateExpected() {
 		return dateExpected;
 	}
 	
-	/**
-	 * Method to get the purchase order's creator
-	 * @return returns Employee data type
-	 */
+
 	public Employee getEmployee() {
 		return employee;
 	}
 	
-	/**
-	 * Method to get the purchase order's current status
-	 * @return returns PurchaseOrderStatus data type
-	 */
+
 	public PurchaseOrderStatus getPurchaseOrderStatus() {
 		return purchaseOrderStatus;
 	}
 	
-	/**
-	 * Method to get the purchase order's supplier
-	 * @return returns Supplier data type
-	 */
+
 	public Supplier getSupplier() {
 		return supplier;
 	}
 	
 	
-	/**
-	 * Method to set the expected date of arrival for the purchase order
-	 * @param dE : Date of predicted arrival time
-	 */
 	public void setDateExpected(Date dE) {
 		dateExpected = dE;
 	}
 	
-	/**
-	 * Method to set the employee working on this order
-	 * @param e : Employee to work on the order
-	 */
+
 	public void setEmployee(Employee e) {
 		employee = e;
 	}
 	
-	/**
-	 * Method to set the current status of the purchase order
-	 * @param pOS : Status of the purchase order
-	 */
+
 	public void setPurchaseOrderStatus(PurchaseOrderStatus pOS) {
 		purchaseOrderStatus = pOS;
 	}
