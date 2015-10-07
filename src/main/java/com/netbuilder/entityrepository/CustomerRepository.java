2c19518df2795ae0141aa39263e9d9ed5ac2b451
c33-cooper <Callum.Cooper@netbuilder.com>
2015-10-07 15:59:09 +0100

    Updated CustomerRepository ready for correct find by conventions

--- src/main/java/com/netbuilder/entityrepository/CustomerRepository.java
+++ src/main/java/com/netbuilder/entityrepository/CustomerRepository.java
@@ -5,6 +5,7 @@
 
 package com.netbuilder.entityrepository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
@@ -19,8 +20,7 @@ import com.netbuilder.entities.Customer;
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
 
 	public Customer findByName(String name);
-	public List<Customer> getCustomers();
-	public List<Customer> getDob();
-	public List<Customer> getPhoneNumber();
-	public List<Customer> getBlackListStrikes();
+	public List<Customer> findByCustomers(Customer c);
+	public List<Customer> findByDob(Date dob);
+	public List<Customer> findByPhoneNumber(String phoneNumber);
 }
