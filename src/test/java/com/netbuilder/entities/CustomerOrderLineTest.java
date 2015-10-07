65c19b9905c872422b4fa7bc4e0110f497fbd108
CoolName1993 <caboucher1993@gmail.com>
2015-10-07 10:42:45 +0100

    Finished test classes

--- src/test/java/com/netbuilder/entities/CustomerOrderLineTest.java
+++ src/test/java/com/netbuilder/entities/CustomerOrderLineTest.java
@@ -13,9 +13,78 @@ import org.junit.Test;
  */
 public class CustomerOrderLineTest {
 
+	private int quantity = 1;
+	private int idItem = 2;
+	private int idCustomerOrder = 3;
+
+	/**
+	 * Test method for
+	 * {@link com.netbuilder.entities.CustomerOrderLine#getQuantity()}.
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetQuantity() {
+		CustomerOrderLine testClass = new CustomerOrderLine(quantity, idItem,
+				idCustomerOrder);
+		assertEquals("Should return 1", 1, testClass.getQuantity());
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.netbuilder.entities.CustomerOrderLine#setQuantity(int)}.
+	 */
+	@Test
+	public void testSetQuantity() {
+		CustomerOrderLine testClass = new CustomerOrderLine(quantity, idItem,
+				idCustomerOrder);
+		testClass.setQuantity(11);
+		assertEquals("Should return 11", 11, testClass.getQuantity());
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.netbuilder.entities.CustomerOrderLine#getIdItem()}.
+	 */
+	@Test
+	public void testGetIdItem() {
+		CustomerOrderLine testClass = new CustomerOrderLine(quantity, idItem,
+				idCustomerOrder);
+		assertEquals("Should return 2", 2, testClass.getIdItem());
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.netbuilder.entities.CustomerOrderLine#setIdItem(int)}.
+	 */
+	@Test
+	public void testSetIdItem() {
+		CustomerOrderLine testClass = new CustomerOrderLine(quantity, idItem,
+				idCustomerOrder);
+		testClass.setIdItem(22);
+		assertEquals("Should return 22", 22, testClass.getIdItem());
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.netbuilder.entities.CustomerOrderLine#getIdCustomerOrder()}.
+	 */
+	@Test
+	public void testGetIdCustomerOrder() {
+		CustomerOrderLine testClass = new CustomerOrderLine(quantity, idItem,
+				idCustomerOrder);
+		assertEquals("Should return 3", 3, testClass.getIdCustomerOrder());
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.netbuilder.entities.CustomerOrderLine#setIdCustomerOrder(int)}
+	 * .
+	 */
+	@Test
+	public void testSetIdCustomerOrder() {
+		CustomerOrderLine testClass = new CustomerOrderLine(quantity, idItem,
+				idCustomerOrder);
+		testClass.setIdCustomerOrder(33);
+		assertEquals("Should return 33", 33, testClass.getIdCustomerOrder());
 	}
 
 }
--- src/test/java/com/netbuilder/entities/PurchaseOrderLineTest.java
+++ src/test/java/com/netbuilder/entities/PurchaseOrderLineTest.java
@@ -9,13 +9,82 @@ import org.junit.Test;
 
 /**
  * @author cboucher
- * @date 07/10/2015
+ *
  */
 public class PurchaseOrderLineTest {
 
+	private int quantity = 1;
+	private int idItem = 2;
+	private int idPurchaseOrder = 3;
+
+	/**
+	 * Test method for
+	 * {@link com.netbuilder.entities.PurchaseOrderLine#getQuantity()}.
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetQuantity() {
+		PurchaseOrderLine testClass = new PurchaseOrderLine(quantity, idItem,
+				idPurchaseOrder);
+		assertEquals("Should return 1", 1, testClass.getQuantity());
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.netbuilder.entities.PurchaseOrderLine#setQuantity(int)}.
+	 */
+	@Test
+	public void testSetQuantity() {
+		PurchaseOrderLine testClass = new PurchaseOrderLine(quantity, idItem,
+				idPurchaseOrder);
+		testClass.setQuantity(11);
+		assertEquals("Should return 11", 11, testClass.getQuantity());
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.netbuilder.entities.PurchaseOrderLine#getIdItem()}.
+	 */
+	@Test
+	public void testGetIdItem() {
+		PurchaseOrderLine testClass = new PurchaseOrderLine(quantity, idItem,
+				idPurchaseOrder);
+		assertEquals("Should return 2", 2, testClass.getIdItem());
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.netbuilder.entities.PurchaseOrderLine#setIdItem(int)}.
+	 */
+	@Test
+	public void testSetIdItem() {
+		PurchaseOrderLine testClass = new PurchaseOrderLine(quantity, idItem,
+				idPurchaseOrder);
+		testClass.setIdItem(22);
+		assertEquals("Should return 22", 22, testClass.getIdItem());
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.netbuilder.entities.PurchaseOrderLine#getIdPurchaseOrder()}.
+	 */
+	@Test
+	public void testGetIdPurchaseOrder() {
+		PurchaseOrderLine testClass = new PurchaseOrderLine(quantity, idItem,
+				idPurchaseOrder);
+		assertEquals("Should return 3", 3, testClass.getIdPurchaseOrder());
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.netbuilder.entities.PurchaseOrderLine#setIdPurchaseOrder(int)}
+	 * .
+	 */
+	@Test
+	public void testSetIdPurchaseOrder() {
+		PurchaseOrderLine testClass = new PurchaseOrderLine(quantity, idItem,
+				idPurchaseOrder);
+		testClass.setIdPurchaseOrder(33);
+		assertEquals("Should return 33", 33, testClass.getIdPurchaseOrder());
 	}
 
 }
