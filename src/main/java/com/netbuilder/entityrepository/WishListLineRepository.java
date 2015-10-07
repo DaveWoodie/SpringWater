0e4ac4564aa88eb0ebda6bd87b06c429661d7ef0
c33-cooper <Callum.Cooper@netbuilder.com>
2015-10-07 16:41:53 +0100

    WishListLineRepository implemented

--- src/main/java/com/netbuilder/entityrepository/WishListLineRepository.java
+++ src/main/java/com/netbuilder/entityrepository/WishListLineRepository.java
@@ -1,5 +1,24 @@
+/**
+ * @author ccooper
+ * @date 07/10/2015
+ */
 package com.netbuilder.entityrepository;
 
-public interface WishListLineRepository {
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.netbuilder.entities.WishListLine;
+import com.netbuilder.entities.Customer;
+import com.netbuilder.entities.CustomerOrder;
+
+/**
+ * 
+ * Interface to hold the find the wish list lines
+ */
+public interface WishListLineRepository extends MongoRepository<WishListLine, Integer>{
+	
+	public WishListLine findByItemID(int id);
+	public List<CustomerOrder> findByCustomerID(Customer c);
 
 }
