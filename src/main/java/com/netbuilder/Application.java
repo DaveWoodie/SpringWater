12c9fad07bb51af041d684a7c17ddfef7620af23
gmwestwater <gmwestwater@hotmail.co.uk>
2015-10-08 15:52:41 +0100

    updated pom.xml

--- src/main/java/com/netbuilder/Application.java
+++ src/main/java/com/netbuilder/Application.java
@@ -4,28 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
 import com.netbuilder.entities.Role;
 import com.netbuilder.entityrepository.RoleRepository;
 
+@Configuration
 @SpringBootApplication
-public class Application implements CommandLineRunner{
+public class Application{
 	
 	@Autowired
 	private RoleRepository repo;
 
 	public static void main(String[] args){
 		SpringApplication.run(Application.class, args);
-	}
-	
-	@Override
-	public void run(String... arg0) throws Exception {
-		repo.deleteAll();
-		repo.save(new Role(0, "Warehouse Manager"));
-		
-		for(Role r : repo.findAll()){
-			System.out.println(r);
-		}
 	}
 
 }
