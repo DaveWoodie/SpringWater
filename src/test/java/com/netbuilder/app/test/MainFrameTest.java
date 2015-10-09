6fa9f29fcc7b4f534b943de67760b5bda900a129
jameshforster <jamezx@hotmail.co.uk>
2015-10-09 14:58:31 +0100

    Main Frame created and layed out

---
+++ src/test/java/com/netbuilder/app/test/MainFrameTest.java
@@ -0,0 +1,11 @@
+package com.netbuilder.app.test;
+
+import com.netbuilder.app.MainFrame;
+
+public class MainFrameTest {
+	
+	public static void main(String[] args) {
+		MainFrame mF = new MainFrame();
+		mF.setVisible(true);
+	}
+}
