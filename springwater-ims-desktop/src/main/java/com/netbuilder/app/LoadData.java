b759780587842f9fef9cb9f45f49c3667445e4f7
jameshforster <jamezx@hotmail.co.uk>
2015-10-14 12:16:31 +0100

    Added test data for inventory GUI

--- springwater-ims-desktop/src/main/java/com/netbuilder/app/LoadData.java
+++ springwater-ims-desktop/src/main/java/com/netbuilder/app/LoadData.java
@@ -53,4 +53,25 @@ public class LoadData {
 		return highSalesItemList;
 	}
 	
+	public Object[][] fetchInventoryList() {
+		
+		Object[][] inventoryList = {{1, "Regular Gnome", 10, "1A", "gnome.jpg"},
+									{2, "Mushroom Gnome", 50, "4B", "gnome02.jpg"},
+									{3, "Lamp Gnome", 80, "7D", "gnome03.jpg"},
+									{4, "Wheelbarrow Gnome", 75, "4A", "gnome04.jpg"},
+									{5, "No Evil Gnomes", 50, "9A", "gnome05.jpg"},
+									{6, "Creepy Gnome", 40, "1F", "gnome06.jpg"},
+									{7, "Large Hoe", 4, "5A", "hoe01.jpg"},
+									{8, "Long Hoe", 25, "5B", "hoe2.jpg"},
+									{9, "Light Jacuzzi", 15, "6A", "jacuzzi01.png"},
+									{10, "Darkwood Jacuzzi", 1, "6B", "jacuzzi02.jpg"},
+									{11, "Blue Step Jacuzzi", 20, "6C", "jacuzzi03.jpg"},
+									{12, "Round Jacuzzi", 0, "6D", "jacuzzi04.png"},
+									{13, "Plain Pot", 20, "8C", "pot01.jpg"},
+									{14, "Patterned Pot", 50, "9D", "pot02.jpg"},
+									{15, "Orange Pot", 60, "3C", "pot03.jpg"}};
+		
+		return inventoryList;
+	}
+	
 }
