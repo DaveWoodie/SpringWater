815a6ef8dfecdf9d7e39def74298cc78f3d47c67
jameshforster <jamezx@hotmail.co.uk>
2015-10-14 11:27:09 +0100

    Added supplier test data loading

--- springwater-ims-desktop/src/main/java/com/netbuilder/app/LoadData.java
+++ springwater-ims-desktop/src/main/java/com/netbuilder/app/LoadData.java
@@ -6,14 +6,34 @@ public class LoadData {
 	
 	public Object[][] fetchPurchaseOrders() {
 		
-		String[][] purchaseOrderList = {{"1", "10/05/2015", "COMPLETED", "GnomeBuilder"},
-								{"2", "17/05/2015", "DELIVERED", "GnomeBuilder"},
-								{"3", "17/05/2015", "AVAILABLE", "GA Inc"},
-								{"4", "17/05/2015", "AVAILABLE", "Garden World"},
-								{"5", "24/05/2015", "AVAILABLE", "GnomeBuilder"},
-								{"6", "24/05/2015", "AVAILABLE", "Lawn Society Suppliers"}};
+		Object[][] purchaseOrderList = {{1, "10/05/2015", "COMPLETED", "GnomeBuilder"},
+								{2, "17/05/2015", "DELIVERED", "GnomeBuilder"},
+								{3, "17/05/2015", "AVAILABLE", "GA Inc"},
+								{4, "17/05/2015", "AVAILABLE", "Garden World"},
+								{5, "24/05/2015", "AVAILABLE", "GnomeBuilder"},
+								{6, "24/05/2015", "AVAILABLE", "Lawn Society Suppliers"}};
 		
 		return purchaseOrderList;
+		
+	}
+	
+	public Object[][] fetchSuppliers() {
+		
+		Object[][] supplierList = {{1, "GnomeBuilder"},
+								{2, "GA Inc"},
+								{3, "Garden World"},
+								{4, "Lawn Society Suppliers"},
+								{5, "Superior Gnomes Ltd"},
+								{6, "Gnomes R' Us"}};
+		
+		return supplierList;
+		
+	}
+	
+	public Object[][] fetchStockReport() {
+		
+		
+		return null;
 		
 	}
 }
