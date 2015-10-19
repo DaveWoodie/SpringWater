0f0468374910ee7984ac154c00ab03fcae6e9d34
AndrewB94 <andrewbutcher94@gmail.com>
2015-10-19 17:32:28 +0100

    work on add supplier

--- springwater-ims-desktop/src/main/java/com/netbuilder/app/AddSupplierFrame.java
+++ springwater-ims-desktop/src/main/java/com/netbuilder/app/AddSupplierFrame.java
@@ -1,3 +1,8 @@
+/**
+ * @author abutcher
+ * @date 16/10/2015
+ */
+
 package com.netbuilder.app;
 
 import java.awt.BorderLayout;
@@ -6,18 +11,24 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import javax.swing.SwingUtilities;
 
+@SuppressWarnings("serial")
 public class AddSupplierFrame extends JFrame {
 	private JPanel base, main, left, right, buttonBar;
 	private JLabel nameL, tPhoneL, eMailL, addressL;
-	private JTextArea nameT, tPhoneT, eMialT;
+	private JTextField nameT, tPhoneT, eMialT;
+	private JButton addSB, addSWPB, cancelB; 
 	
 	public AddSupplierFrame(){
-		System.out.print("Here");
+	//	System.out.print("Here");
 		initUI();
 	}
 	
@@ -28,16 +39,19 @@ public class AddSupplierFrame extends JFrame {
 	
 	private void configFrame(){
 		setTitle("Add New Supplier");
-		setSize(600, 800);
+		setSize(500, 140);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
-        setMinimumSize(new Dimension(600, 800));
+        setResizable(false);
     }
 	
 	private void addContent() {
 		base = new JPanel(new BorderLayout());
 		add(base);
-		main = new JPanel(new GridLayout(1,2));
+//		main = new JPanel(new GridLayout(1,2));
+		main = new JPanel();
+		main.setLayout(new BoxLayout(main, BoxLayout.X_AXIS));
+
 		
 		//Left Panel
 		left = new JPanel();
@@ -51,10 +65,12 @@ public class AddSupplierFrame extends JFrame {
 		
 		//Right Panel
 		right = new JPanel();
-		right.setLayout(new BoxLayout(right, BoxLayout.Y_AXIS));
-		nameT = new JTextArea();
-		tPhoneT = new JTextArea();
-		eMialT = new JTextArea();
+		nameT = new JTextField();
+		nameT.setColumns(30);
+		tPhoneT = new JTextField();
+		tPhoneT.setColumns(30);
+		eMialT = new JTextField();
+		eMialT.setColumns(30);
 		right.add(nameT);
 		right.add(tPhoneT);
 		right.add(eMialT);
@@ -62,6 +78,18 @@ public class AddSupplierFrame extends JFrame {
 		main.add(left);
 		main.add(right);
 		
+		//Button Bar Panel
+		buttonBar = new JPanel();
+		buttonBar.setLayout(new GridLayout(1, 3));
+		addSB = new JButton("Add");
+		addSWPB = new JButton("Add (With Products)");
+		cancelB = new JButton("Cancel");
+		
+		buttonBar.add(addSB);
+		buttonBar.add(addSWPB);
+		buttonBar.add(cancelB);
+		
 		base.add(main, BorderLayout.CENTER);
+		base.add(buttonBar, BorderLayout.SOUTH);
 	}
 }
