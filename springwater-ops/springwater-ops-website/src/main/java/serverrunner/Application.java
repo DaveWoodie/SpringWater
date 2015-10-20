eddd2fbd710b424f6d89a4c29a27ad184894bfa6
gmwestwater <gmwestwater@hotmail.co.uk>
2015-10-20 17:29:12 +0100

    bean search in application.java

--- springwater-ops/springwater-ops-website/src/main/java/serverrunner/Application.java
+++ springwater-ops/springwater-ops-website/src/main/java/serverrunner/Application.java
@@ -4,6 +4,8 @@ package serverrunner;
 import javax.servlet.ServletException;
 
 import org.apache.catalina.LifecycleException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,17 +23,22 @@ public class Application  {
 	public static void main(String args[]) throws ServletException, LifecycleException {
 
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
-		SpringApplication.run(Application.class, args);
+		
+		//listBeans(ctx);
 	}
 	
-	/*
+	
 	//Lists all beans active in the application context
 	//Use to view if a specific bean has been created
-	public void listBeans(ApplicationContext ctx)
+	public static void listBeans(ApplicationContext ctx)
 	{
+		String s;
+		
 		for(int i = 0; i < ctx.getBeanDefinitionCount(); i++)
 		{
-			System.out.println("------> " + ctx.getBeanDefinitionNames()[i]);
+			s = (ctx.getBeanDefinitionNames()[i]);
+			
+			System.out.println("------> Bean " + (i + 1) + " : " + s);
 		}
-	}*/
+	}
 }
