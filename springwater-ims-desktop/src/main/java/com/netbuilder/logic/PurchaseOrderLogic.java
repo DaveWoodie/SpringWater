6ef451db605e99b2b787e0886b4ef467a23d69f9
jameshforster <jamezx@hotmail.co.uk>
2015-10-21 11:45:13 +0100

    Started work on the PurchaseOrderLogic

--- springwater-ims-desktop/src/main/java/com/netbuilder/logic/PurchaseOrderLogic.java
+++ springwater-ims-desktop/src/main/java/com/netbuilder/logic/PurchaseOrderLogic.java
@@ -1,5 +1,30 @@
 package com.netbuilder.logic;
 
-public class PurchaseOrderLogic {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
+import com.netbuilder.apploader.PurchaseOrderLoader;
+import com.netbuilder.entities.PurchaseOrder;
+
+public class PurchaseOrderLogic {
+	
+	Object [][] purchaseOrderList;
+	PurchaseOrderLoader pOLoader;
+	DateFormat df = new SimpleDateFormat("dd/MM/yy");
+	
+	public Object [][] fetchPurchaseOrders() {
+		ArrayList<PurchaseOrder> pOList = new ArrayList<PurchaseOrder>(pOLoader.getPurchaseOrderList());
+		float total = 3160;
+		//TODO connect to OrderLineLoader and ItemLoader to calculate price
+		purchaseOrderList = new Object [pOList.size()][4];
+		for (int i = 0; i < pOList.size(); i++) {
+			purchaseOrderList[i][0] = pOList.get(i).getIDPurchaseOrder();
+			purchaseOrderList[i][1] = df.format(pOList.get(i).getDatePlaced()); 
+			purchaseOrderList[i][2] = pOList.get(i).getPurchaseOrderStatus().getPurchOrderStatus(); 
+			purchaseOrderList[i][3] = pOList.get(i).getSupplier().getSupplierName();
+			purchaseOrderList[i][4] = "£" + total;
+		}
+		return null;
+	}
 }
