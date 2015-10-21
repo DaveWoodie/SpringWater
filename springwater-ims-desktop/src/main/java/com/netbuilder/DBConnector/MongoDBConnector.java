0f6fafda59fc9eed91f8524099af2c69b464ae52
chrisjwwalker <cjwa@walker54.plus.com>
2015-10-21 14:55:01 +0100

    Added IP address, user and pass to mongodb connector

--- springwater-ims-desktop/src/main/java/com/netbuilder/DBConnector/MongoDBConnector.java
+++ springwater-ims-desktop/src/main/java/com/netbuilder/DBConnector/MongoDBConnector.java
@@ -1,6 +1,7 @@
 package com.netbuilder.DBConnector;
 
 import java.net.UnknownHostException;
+import java.util.Set;
 
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -10,30 +11,38 @@ import com.mongodb.MongoClient;
 public class MongoDBConnector {
 
 	static final String databaseURL = null;
-	static final String userName = null;
-	static final String passWord = null;
+	static final String userName = "admin";
+	static final String passWord = "netbuilder";
 	
 	private String host = "localhost";
 	
 	
-	public void openCon() {
+	public static void openCon() {
 		
 		try {
 			
 			//Connect to the host localhost through the default port 27017.
-			Mongo mongo = new Mongo("localhost", 27017);
+			Mongo mongo = new Mongo("10.50.15.27", 27017);
 		
 			MongoClient mongoClient = new MongoClient();
-			DB db = mongoClient.getDB("database name");
-			boolean auth = db.authenticate("username", "password".toCharArray());
+			DB db = mongoClient.getDB("nbgardensdata");
+			boolean auth = db.authenticate(userName, passWord.toCharArray());
 			
 			//To get a table or collection from a Mongo DB
 			DBCollection table = db.getCollection("user");
+			
+			Set<String> collections = db.getCollectionNames();
+
+			for (String collectionName : collections) {
+				System.out.println(collectionName);
+			}
 			
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	
+	public static void main(String [] args) {
+		openCon();
+	}
 }
