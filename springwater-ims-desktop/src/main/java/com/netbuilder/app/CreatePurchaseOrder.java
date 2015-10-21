e768bbee05582f42646abf959e3dc2be4bd3cad3
gmwestwater <gmwestwater@hotmail.co.uk>
2015-10-21 12:35:35 +0100

    erd v2

--- springwater-ims-desktop/src/main/java/com/netbuilder/app/CreatePurchaseOrder.java
+++ springwater-ims-desktop/src/main/java/com/netbuilder/app/CreatePurchaseOrder.java
@@ -1,5 +1,32 @@
 package com.netbuilder.app;
 
-public class CreatePurchaseOrder {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+@SuppressWarnings("serial")
+public class CreatePurchaseOrder extends JPanel
+{	
+	public JPanel createUI()
+	{
+		
+		
+		
+		return this;
+	}
+	
+
+	public static void main(String[] args)
+	{
+		JFrame j = new JFrame();
+		
+		CreatePurchaseOrder cpo = new CreatePurchaseOrder();
+		
+		j.add(cpo.createUI());
+		
+		j.setSize(600, 800);
+		j.setLocationRelativeTo(null);
+		j.setVisible(true);
+		
+		System.out.println("Frame set up");
+	}
 }
