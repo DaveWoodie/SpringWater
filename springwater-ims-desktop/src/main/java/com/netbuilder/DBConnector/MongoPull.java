dd7b064023f9232b3fc70b2bf610eda18216e439
chrisjwwalker <cjwa@walker54.plus.com>
2015-10-22 14:40:17 +0100

    Updated MongoDBConnector and MongoPull with JavaDocs information

--- springwater-ims-desktop/src/main/java/com/netbuilder/DBConnector/MongoPull.java
+++ springwater-ims-desktop/src/main/java/com/netbuilder/DBConnector/MongoPull.java
@@ -1,3 +1,8 @@
+/**
+ * @author chrisjwwalker
+ * @date 22/10/2015
+ */
+
 package com.netbuilder.DBConnector;
 
 import java.util.ArrayList;
@@ -12,20 +17,31 @@ import com.mongodb.DBCursor;
 
 public class MongoPull {
 
+	//Create Instance of Mongo Connector to connect to DB
 	private MongoDBConnector mdbc = new MongoDBConnector();
 	
+	//Strings specifying database and collections to use
 	private final String dataBase = "nbgardensdata";
 	private final String AddrCol = "Address";
 	private final String itemCol = "Item";
 	private final String wishListCol = "WishList";
 	
+	//Lists returned by the following methods
 	private List<String> addrVals = new ArrayList<String>();
 	private List<String> itemInfs = new ArrayList<String>();
+	private List<String> wishListSet = new ArrayList<String>();
 
 	public MongoPull() {
 
 	}
 	
+	/**
+	 * 
+	 * Method to fetch an address based on an address ID
+	 * 
+	 * @param id: Takes an int which is the address ID
+	 * @return returns a List containing all the lines of address associated with the ID
+	 */
 	public List<String> getAddress(int id) {
 		//Connect to MongoDB
 		mdbc.mongoConnect();
@@ -65,6 +81,13 @@ public class MongoPull {
 		return addrVals;
 	}
 	
+	/**
+	 * 
+	 * Method to fetch all the information stored on a given item based on idItem
+	 * 
+	 * @param id: Takes an int which is the idItem of a given item
+	 * @return returns a list containing all the information for the given item
+	 */
 	public List<String> getItemInf(int id) {
 		//Connect to MongoDB
 		mdbc.mongoConnect();
@@ -112,5 +135,48 @@ public class MongoPull {
 		mdbc.mongoDisconnect();
 		
 		return itemInfs;
+	}
+	
+	/**
+	 * 
+	 * Method to fetch the contents of a customers wish list
+	 * 
+	 * @param id : Takes an int which is the idCustomer of a customer 
+	 * @return returns a List containing the items stored within a customers wish list 
+	 */
+	public List<String> getWishList(int id) {
+		//Connect to MongoDB
+		mdbc.mongoConnect();
+		
+		//Connect to the NBGardensn database
+		DB db = mdbc.getConnection().getDB(dataBase);
+		//Get Specfic Collection
+		DBCollection collection = db.getCollection(wishListCol);
+		
+		BasicDBObject wL = new BasicDBObject();
+		wL.put("idCustomer", id);
+		DBCursor cursor = collection.find(wL);		
+		
+		wishListSet.clear();
+		
+		while(cursor.hasNext()) {
+			
+			cursor.next();
+			
+			BSONObject bsobj = (BSONObject) cursor.curr().get("items");
+			int i = 1; 
+			while(bsobj.containsField("idItem" + i)) {
+				wishListSet.add(bsobj.get("idItem" + i).toString());
+				
+				i++;
+			}
+		}
+		
+		cursor.close();
+				
+		//Disconnect from MongoDB
+		mdbc.mongoDisconnect();
+		
+		return wishListSet;
 	}
 }
