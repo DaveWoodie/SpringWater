2a059896d4066bdb77e6b36df6660a91aca391c4
chrisjwwalker <cjwa@walker54.plus.com>
2015-10-22 12:28:34 +0100

    Ability to pull ANY address from MongoDB using an ID

--- springwater-ims-desktop/src/main/java/com/netbuilder/DBConnector/MongoDataTest.java
+++ springwater-ims-desktop/src/main/java/com/netbuilder/DBConnector/MongoDataTest.java
@@ -1,10 +1,19 @@
 package com.netbuilder.DBConnector;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MongoDataTest {
 
 	public static void main(String [] args) {
 		MongoPull mp = new MongoPull();
 		
-		mp.find("Address", "idAddress","0.0", "","");
+		List<String> vals = mp.getAddress(3);
+		
+		for(String val : vals) {
+			System.out.println(val);
+		}
+		
+		//System.out.println(mp.canItBeParsed("Chris"));
 	}
 }
