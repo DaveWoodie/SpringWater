773eb9aa3d77761b3cedac678df7bf4053233fb5
CoolName1993 <caboucher1993@gmail.com>
2015-10-22 15:00:32 +0100

    added test item entity and got items page to display dummy data

--- springwater-ops/springwater-ops-website/src/main/java/com/netbuilder/controller/ItemsController.java
+++ springwater-ops/springwater-ops-website/src/main/java/com/netbuilder/controller/ItemsController.java
@@ -1,30 +1,27 @@
-//package com.netbuilder.controller;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import com.netbuilder.entities.Item;
-//import com.netbuilder.entityrepository.ItemRepository;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//
-//@Controller
-//public class ItemsController {
-//
-//	private List<Item> items;
-//
-//	@Autowired
-//	ItemRepository itemRepository;
-//
-//	@RequestMapping(value = "/items", method = RequestMethod.GET)
-//	public String greetingForm(Model model) {
-//		items = itemRepository.findAll();
-//		System.out.println("hello");
-//		model.addAttribute("itemList", items);
-//		return "items";
-//	}
-//}
+package com.netbuilder.controller;
+
+import java.util.ArrayList;
+
+import com.netbuilder.test.Item;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ItemsController {
+
+	private ArrayList<Item> itemList;
+	
+	@RequestMapping(value = "/items", method = RequestMethod.GET)
+	public String itemTest(Model model) {
+		itemList = new ArrayList<Item>();
+		itemList.add(new Item("itemName", 10.00f, "img/hoe01.jpg", 3));
+		itemList.add(new Item("itemName2", 11.00f, "img/hoe2.jpg", 2));
+		itemList.add(new Item("itemName3", 12.00f, "img/gnome.jpg", 4));
+		itemList.add(new Item("itemName4", 13.00f, "img/gnome02.jpg", 5));
+		model.addAttribute("itemList",itemList);
+		return "items";
+	}
+}
