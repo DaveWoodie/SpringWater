619f45297f5a2748529a36278224a7469acee274
chrisjwwalker <cjwa@walker54.plus.com>
2015-10-23 15:33:43 +0100

    Started email order form; JSP & JS

--- springwater-ops/springwater-ops-website/src/main/webapp/WEB-INF/jsp/emailUpdated.js
+++ springwater-ops/springwater-ops-website/src/main/webapp/WEB-INF/jsp/emailUpdated.js
@@ -1,3 +1,9 @@
-function updateAddr() {
+var answers = [];
+
+function updateAddr(ID) {
+    for(var n = 0; n < document.getElementById(ID).value.length; n++) {
+        answers[ID] = document.getElementById(ID).value;
+    }
     
+    document.getElementById('A'+ID).innerHTML = answers[ID];
 }
