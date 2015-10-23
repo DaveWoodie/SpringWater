4ecb5075c460eea546fc13eaa55b73efec17eddf
gmwestwater <gmwestwater@hotmail.co.uk>
2015-10-23 09:18:14 +0100

    db

---
+++ springwater-ims-desktop/src/main/java/com/netbuilder/apploader/ItemLoader.java
@@ -0,0 +1,23 @@
+package com.netbuilder.apploader;
+
+import java.util.ArrayList;
+
+import com.netbuilder.DBConnector.MongoPull;
+import com.netbuilder.entities.Item;
+
+public class ItemLoader 
+{
+	private MongoPull mongoPull;
+	private ArrayList<Item> itemArray;
+	
+	public ItemLoader()
+	{
+		mongoPull = new MongoPull();
+	}
+	
+	public ArrayList<Item> loadItemByID(int itemID)
+	{
+		itemArray = mongoPull.getItemInf(itemID);
+		return itemArray;
+	}
+}
