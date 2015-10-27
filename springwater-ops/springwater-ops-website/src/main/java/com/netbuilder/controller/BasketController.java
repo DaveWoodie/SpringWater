e3f81bbf2a53921bcdbe4996c68e168e4ab01ff9
c33-cooper <c33-cooper>
2015-10-27 14:38:11 +0000

    Updated basket controller

--- springwater-ops/springwater-ops-website/src/main/java/com/netbuilder/controller/BasketController.java
+++ springwater-ops/springwater-ops-website/src/main/java/com/netbuilder/controller/BasketController.java
@@ -21,56 +21,7 @@ import com.netbuilder.test.ItemDatabase;
 import com.netbuilder.test.Review;
 
 @Controller
+@RequestMapping("/")
 public class BasketController {
-	
-	@RequestMapping(value = "/basketpage+*", method = RequestMethod.GET)
-	public String getItemInBasket(HttpServletRequest request, Model model) {
-		
-		// Get current URL and compare to database
-		String url = (String) request
-				.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
-		String search = url.replace("/basketpage+", "");
-		Item result = ItemDatabase.searchID(search);
-		
-		// IF the item exists
-				if(result != null) {
-					
-					// Get related Items
-					ArrayList<Item> resultList = ItemDatabase.searchKeyword(result.getKeyword());
-					ArrayList<Item> relatedList = new ArrayList<Item>();
-					int deleteIndex = 0;
-					for(Item item : resultList)
-					{
-						if(item.getItemID().equals(result.getItemID()))
-						{
-							deleteIndex = resultList.indexOf(item);
-						}
-					}
-					resultList.remove(deleteIndex);
-					Random rand = new Random();
-					int index = rand.nextInt(resultList.size());
-					relatedList.add(resultList.get(index));
-					index = rand.nextInt(resultList.size());
-					relatedList.add(resultList.get(index));
-					index = rand.nextInt(resultList.size());
-					relatedList.add(resultList.get(index));
-					index = rand.nextInt(resultList.size());
-					relatedList.add(resultList.get(index));
-					
-					// Get the reviews for the item
-					ArrayList<Review> reviewList = result.getReviews();
-					
-					// Add the data to the web page
-					model.addAttribute("item", result);
-					model.addAttribute("relatedList", relatedList);
-					model.addAttribute("reviewList", reviewList);
-					return "itemPage";
-				}
-				
-				// Return an error page
-				else
-				{
-					return "errorPage";
-				}
-			}
-	}
+
+}
