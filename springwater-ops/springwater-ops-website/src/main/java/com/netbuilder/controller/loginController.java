a4790cbbffc6d48cb953498b6c1c4a72609d8467
DaveWoodie <David.Woodward@netbuilder.com>
2015-10-27 14:35:42 +0000

    Added the re-direct to the login page.

--- springwater-ops/springwater-ops-website/src/main/java/com/netbuilder/controller/loginController.java
+++ springwater-ops/springwater-ops-website/src/main/java/com/netbuilder/controller/loginController.java
@@ -9,12 +9,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import encryption.EncryptPassword;
+
 @Controller
 public class loginController {
 	
 	@RequestMapping(value = "loginForm", method = RequestMethod.POST)
 	public String doPost(HttpServletRequest request, HttpServletResponse response) {
 
+		String returned = "redirect:/loginregister";
+		EncryptPassword n = new EncryptPassword(); 
+		
 		String email = request.getParameter("emailAddressInput");
 		String passwd = request.getParameter("passwordInputLogin");
 		
@@ -30,10 +35,14 @@ public class loginController {
 			System.out.println(s[0]);
 			System.out.println(s[1]);
 			
+			if (s[0].equals(email) && s[1].equals(n.checkSHA1(passwd))) { 
+				returned = "redirect:";
+			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
-		return "redirect:/loginregister";
+		return returned;
 	}
 }
