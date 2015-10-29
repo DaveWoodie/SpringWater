4dd8a368ee1a282748b9b5e9eeb8c656541c769c
AndrewB94 <andrewbutcher94@gmail.com>
2015-10-29 15:52:16 +0000

    supplier gui / logic / loader

--- springwater-ops/springwater-ops-website/src/main/java/loaders/SupplierLoader.java
+++ springwater-ops/springwater-ops-website/src/main/java/loaders/SupplierLoader.java
@@ -47,6 +47,7 @@ public class SupplierLoader {
 				s.setEmail(rs.getString("email"));
 				s.setTelephone(rs.getString("telephoneNumber"));
 				s.setSupplierID(rs.getInt("idSupplier"));
+				s.setImageLocation(rs.getString("image"));
 				int av = averageDelivery(s.getSupplierID());
 				s.setAverageDeliveryTime(av);
 				supplierList.add(s);
@@ -146,42 +147,41 @@ public class SupplierLoader {
 	 */
 	public void newSupplier(Supplier newSupplier) {
 		System.out.println(newSupplier.getImageLocation());
-//		if (!newSupplier.getEmail().isEmpty() && !newSupplier.getTelephone().isEmpty()) {
-//			sql = insertQuery + collumnsWTE + newSupplier.getSupplierName() +"', '"+ newSupplier.getTelephone() +"', '"+ newSupplier.getEmail()+"', '"+ newSupplier.getAddressID()+"', '"+newSupplier.getImageLocation() +"')";
-//		}else if(!newSupplier.getEmail().isEmpty()) {
-//			sql = insertQuery + collumnsWE + newSupplier.getSupplierName() +"', '"+ newSupplier.getEmail()+"', '"+ newSupplier.getAddressID()+"', '"+newSupplier.getImageLocation() +"')";
-//		}else {
-//			sql = insertQuery + collumnsWT + newSupplier.getSupplierName() +"', '"+ newSupplier.getTelephone() +"', '"+ newSupplier.getAddressID()+"', '"+newSupplier.getImageLocation() +"')";
-//		}
-//		sqlDB.openCon();
-//		try {
-//			sqlDB.updateDB(sql);
-//		} 
-//		catch (SQLException sqle) {
-//			sqle.printStackTrace();
-//			push.deleteAddressByID(newSupplier.getAddressID());
-//			JFrame frame = new JFrame("Could not add supplier");
-//			JOptionPane.showMessageDialog(frame, "Supplier has been updated \n SQL Error", "Warning",
-//					JOptionPane.ERROR_MESSAGE);
-//		}
-//		catch (Exception e) {
-//			e.printStackTrace();
-//			push.deleteAddressByID(newSupplier.getAddressID());
-//			JFrame frame = new JFrame("Could not add supplier");
-//			JOptionPane.showMessageDialog(frame, "Supplier has been updated", "Warning",
-//					JOptionPane.ERROR_MESSAGE);
-//		}
-//		sqlDB.closeCon();
-		push.deleteAddressByID(newSupplier.getAddressID());
+		if (!newSupplier.getEmail().isEmpty() && !newSupplier.getTelephone().isEmpty()) {
+			sql = insertQuery + collumnsWTE + newSupplier.getSupplierName() +"', '"+ newSupplier.getTelephone() +"', '"+ newSupplier.getEmail()+"', '"+ newSupplier.getAddressID()+"', '"+newSupplier.getImageLocation() +"')";
+		}else if(!newSupplier.getEmail().isEmpty()) {
+			sql = insertQuery + collumnsWE + newSupplier.getSupplierName() +"', '"+ newSupplier.getEmail()+"', '"+ newSupplier.getAddressID()+"', '"+newSupplier.getImageLocation() +"')";
+		}else {
+			sql = insertQuery + collumnsWT + newSupplier.getSupplierName() +"', '"+ newSupplier.getTelephone() +"', '"+ newSupplier.getAddressID()+"', '"+newSupplier.getImageLocation() +"')";
+		}
+		sqlDB.openCon();
+		try {
+			sqlDB.updateDB(sql);
+		} 
+		catch (SQLException sqle) {
+			sqle.printStackTrace();
+			push.deleteAddressByID(newSupplier.getAddressID());
+			JFrame frame = new JFrame("Could not add supplier");
+			JOptionPane.showMessageDialog(frame, "Supplier has been updated \n SQL Error", "Warning",
+					JOptionPane.ERROR_MESSAGE);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			push.deleteAddressByID(newSupplier.getAddressID());
+			JFrame frame = new JFrame("Could not add supplier");
+			JOptionPane.showMessageDialog(frame, "Supplier has been updated", "Warning",
+					JOptionPane.ERROR_MESSAGE);
+		}
+		sqlDB.closeCon();
 	}
 
 	public void updateSupplier(Supplier upSP) {
 		if (!upSP.getEmail().isEmpty() && !upSP.getTelephone().isEmpty()) {
-		sql = update+ upSP.getSupplierName()+ "', telephoneNumber ='"+upSP.getTelephone() + "', email ='" + upSP.getEmail() +"', image ='"+upSP.getImageLocation() + "' WHERE `idSupplier`='"+upSP.getSupplierID()+"';";
+		sql = update+ upSP.getSupplierName()+ "', telephoneNumber ='"+upSP.getTelephone() + "', email ='" + upSP.getEmail() + "' WHERE `idSupplier`='"+upSP.getSupplierID()+"';";
 		} else if (!upSP.getEmail().isEmpty()) {
-			sql = update+ upSP.getSupplierName()+ "', email ='" + upSP.getEmail() +"', image ='"+upSP.getImageLocation() + "' WHERE `idSupplier`='"+upSP.getSupplierID()+"';";
+			sql = update+ upSP.getSupplierName()+ "', email ='" + upSP.getEmail() + "' WHERE `idSupplier`='"+upSP.getSupplierID()+"';";
 		} else {
-			sql = update+ upSP.getSupplierName()+ "', telephoneNumber ='"+upSP.getTelephone() + "', image ='"+upSP.getImageLocation() + "' WHERE `idSupplier`='"+upSP.getSupplierID()+"';";
+			sql = update+ upSP.getSupplierName()+ "', telephoneNumber ='"+upSP.getTelephone() + "' WHERE `idSupplier`='"+upSP.getSupplierID()+"';";
 		}
 		sqlDB.openCon();
 		try {
