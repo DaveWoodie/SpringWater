e8dc8dfc99254170ab704c0241a8a201d1b22e60
jameshforster <jamezx@hotmail.co.uk>
2015-10-30 09:34:35 +0000

    Added new customer order stock update

--- springwater-ops/springwater-ops-backend/src/main/java/com/netbuilder/logic/CustomerOrderBackendLogic.java
+++ springwater-ops/springwater-ops-backend/src/main/java/com/netbuilder/logic/CustomerOrderBackendLogic.java
@@ -1,10 +1,27 @@
 package com.netbuilder.logic;
 
+import java.util.ArrayList;
+
+import connections.MongoPull;
+import connections.MongoPush;
+import loaders.CustomerOrderLineLoader;
 import entities.CustomerOrder;
+import entities.CustomerOrderLine;
+import entities.Item;
 
 public class CustomerOrderBackendLogic {
 	
 	public void customerOrderPlaced(CustomerOrder cO) {
-		
+		CustomerOrderLineLoader cOLLoader = new CustomerOrderLineLoader();
+		MongoPull mPull = new MongoPull();
+		MongoPush mPush = new MongoPush();
+		ArrayList<CustomerOrderLine> cOLList = new ArrayList<CustomerOrderLine>();
+		Item item = null;
+		cOLList = cOLLoader.getCustomerOrderLineByOrderID(cO.getCustomerOrderID());
+		for (int i = 0; i < cOLList.size(); i++) {
+			item = mPull.getItem(cOLList.get(i).getItemID());
+			item.setStock(item.getStock() - cOLList.get(i).getQuantity());
+			mPush.updateItem(item);
+		}
 	}
 }
